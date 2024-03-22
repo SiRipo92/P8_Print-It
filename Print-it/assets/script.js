6ab854ea3f9940b26a6979919628e6b7df40bde6
SiRipo92 <ripochesierra@gmail.com>
2024-03-22 16:54:19 +0100

    Added event listeners to left and right arrows and console log to check their functioning and that they make distinctions between left and right clicks

--- Print-it/assets/script.js
+++ Print-it/assets/script.js
@@ -16,3 +16,14 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+let arrow_left = document.getElementsByClassName("arrow arrow_left")[0];
+let arrow_right = document.getElementsByClassName("arrow arrow_right")[0];
+
+arrow_left.addEventListener("click", function(){
+	console.log("left click");
+});
+
+arrow_right.addEventListener("click", function(){
+	console.log("right click");
+});
